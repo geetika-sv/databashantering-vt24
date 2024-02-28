b9e98b97a165cc6414a1772ae75a5e2e15996ba6
Geetika-Jain1 <drgeetika332@gmail.com>
2024-02-28 17:19:42 +0100

    create java_mysqlworkbench.sql

---
+++ java_mysqlworkbench.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE java_mysqlworkbench;
+
+USE java_mysqlworkbench;
+
+CREATE TABLE javatable (
+    table_id INT AUTO_INCREMENT PRIMARY KEY,
+    table_name VARCHAR(255),
+    age INT
+    );
